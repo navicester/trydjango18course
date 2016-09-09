0f3f17af7d203955bc294e209fd8666a0a0e6902
navicester <navicester@qq.com>
2016-09-09 08:17:12 +0800

    view and template context

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-	context = {}
-	template = "home.html"
-	return render(request, template, context)
+	title = 'Welcome'
+
+	if request.user.is_authenticated():
+		title = "My title is %s" % (request.user)
+	context = {
+		"title": title,
+	}
+
+	return render(request, "home.html", context)
