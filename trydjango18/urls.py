74e4b21d8bbc9e37b0f843d0893378d3631cc7dd
navicester <navicester@qq.com>
2016-09-11 21:15:22 +0800

    url names as Link

--- trydjango18/urls.py
+++ trydjango18/urls.py
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     # Examples:
     url(r'^$', 'newsletter.views.home', name='home'),
-    url(r'^contact/$', 'newsletter.views.contact', name='contact'),    
+    url(r'^contact/$', 'newsletter.views.contact', name='contact'),   
+    url(r'^about/$', 'trydjango18.views.about', name='about'),
+         
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
