0f1eff52234e77f1e9af03d6c76572defa0cc7d8
navicester <navicester@qq.com>
2016-09-11 14:17:33 +0800

    static file

--- newsletter/views.py
+++ newsletter/views.py
@@ -6,63 +6,74 @@ from .forms import SignUpForm,ContactForm
 from .models import SignUp
 
 def home(request):
-	title = 'Welcome'
-	form = SignUpForm(request.POST or None)
-	context = {
-		"title": title,
-		"form": form		
-	}
-	print request
-	print request.POST
-	
-	if form.is_valid():
-		#form.save()
-		#print request.POST['email'] #not recommended, raw data without validation
-		instance = form.save(commit=False)
+    title = 'Welcome'
+    form = SignUpForm(request.POST or None)
+    context = {
+        "title": title,
+        "form": form        
+    }
+    print request
+    print request.POST
+    
+    if form.is_valid():
+        #form.save()
+        #print request.POST['email'] #not recommended, raw data without validation
+        instance = form.save(commit=False)
 
-		full_name = form.cleaned_data.get("full_name")
-		if not full_name:
-			full_name = "New full name"
-		instance.full_name = full_name
-		# if not instance.full_name:
-		# 	instance.full_name = "Justin"
-		instance.save()
-		context = {
-			"title": "Thank you"
-		}
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        # if not instance.full_name:
+        #     instance.full_name = "Justin"
+        instance.save()
+        context = {
+            "title": "Thank you"
+        }
 
-	return render(request, "home.html", context)
+    return render(request, "home.html", context)
 
 def contact(request):
-	form = ContactForm(request.POST or None)
+    form = ContactForm(request.POST or None)
 
-	if form.is_valid():
-		# for key, value in form.cleaned_data.iteritems():
-		# 	print key, value
-		# 	#print form.cleaned_data.get(key)
+    if form.is_valid():
+        # for key, value in form.cleaned_data.iteritems():
+        #     print key, value
+        #     #print form.cleaned_data.get(key)
 
-		form_email = form.cleaned_data.get("email")
-		form_message = form.cleaned_data.get("message")
-		form_full_name = form.cleaned_data.get("full_name")
-		# print email, message, full_name
-		subject = 'Site contact form'
-		from_email = settings.EMAIL_HOST_USER
-		to_email = [from_email, 'hebinn2004@sina.com']
-		contact_message = "%s: %s via %s"%( 
-				form_full_name, 
-				form_message, 
-				form_email)
-		some_html_message = """
-		<h1>hello</h1>
-		"""
-		send_mail(subject, 
-				contact_message, 
-				from_email, 
-				to_email, 
-				html_message=some_html_message,
-				fail_silently=True)
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'hebinn2004@sina.com']
+        contact_message = "%s: %s via %s"%( 
+                form_full_name, 
+                form_message, 
+                form_email)
+        some_html_message = """
+        <h1>hello</h1>
+        """
 
-	context = {
-		"form": form,	
-	}
-	return render(request, "forms.html", context)
+        send_mail(subject, 
+                contact_message, 
+                from_email, 
+                to_email, 
+                html_message=some_html_message,
+                fail_silently=False)
+
+    context = {
+        "form": form,    
+    }
+    return render(request, "forms.html", context)
+
+        # import smtplib
+        # try:
+        #     smtpObj = smtplib.SMTP() 
+        #     smtpObj.connect(settings.EMAIL_HOST, 25)
+        #     smtpObj.login(settings.EMAIL_HOST_USER,settings.EMAIL_HOST_PASSWORD)  
+        #     smtpObj.sendmail(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_USER, some_html_message)
+        #     print "sent successfully !!!!!!!!!!!!!!!!"
+        # except smtplib.SMTPException:
+        #     print "Error: sent fail $$$$$$$$$$$$$$"
