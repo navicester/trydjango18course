c638f3f9b6476927d2e3b52fb31cd7b0fc100496
navicester <navicester@qq.com>
2016-09-11 21:34:08 +0800

    styling MVP landing part2

--- newsletter/views.py
+++ newsletter/views.py
@@ -35,6 +35,8 @@ def home(request):
     return render(request, "home.html", context)
 
 def contact(request):
+    title = 'Contact Us'    
+    title_align_center = True
     form = ContactForm(request.POST or None)
 
     if form.is_valid():
@@ -65,7 +67,9 @@ def contact(request):
                 fail_silently=False)
 
     context = {
-        "form": form,    
+        "form": form,
+        "title": title,
+        "title_align_center": title_align_center,
     }
     return render(request, "forms.html", context)
 
