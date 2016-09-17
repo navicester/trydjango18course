51a5f8375c35a5d57e089074672e2bf9b7c8839f
navicester <navicester@qq.com>
2016-09-17 16:37:10 +0800

    remove db

--- trydjango18/settings/production.py
+++ trydjango18/settings/production.py
@@ -4,11 +4,11 @@ from django.conf import settings
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-DATABASES = settings.DATABASES
+# DATABASES = settings.DATABASES
 
-import dj_database_url
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# # Parse database configuration from $DATABASE_URL
+# DATABASES['default'] =  dj_database_url.config()
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
